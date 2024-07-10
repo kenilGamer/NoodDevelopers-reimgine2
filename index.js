f9e3a4d5ba5adf46a8c09056a893d1ee9efb156b
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-07-10 14:59:05 +0530

    gbrb

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const locoScroll = new LocomotiveScroll({
   smoothMobile: true,
   inertia: 0.5, 
   multiplier: 0.9, 
-  class: "is-reveal",
+//   class: "is-reveal",
 });
 
 gsap.registerPlugin(ScrollTrigger);
@@ -33,11 +33,60 @@ ScrollTrigger.refresh();
 
 
 function page1(){
-
+    const tl = gsap.timeline()
+    tl.to(".nav-img",{
+    // top: -100,
+    opacity:1,
+    duration: 1,
+    // ease: "power4.inOut"
+    })
+    tl.to(".nav-all",{
+        left:"20%",
+        opacity:1,
+        duration: 1,
+        // ease: "power4.inOut"
+    },'a')
+    tl.to(".nav-daily",{
+        left:"75%",
+        opacity:1,
+        duration: 1,
+        // ease: "power4.inOut"
+    },'a')
+    tl.to(".nav-shop",{
+        left:"5%",
+        duration: 1,
+        opacity:1
+        // ease: "power4.inOut"
+    },'b')
+    tl.to('.nav-more',{
+        left:"92%",
+        opacity:1,
+        duration: 1,
+        // ease: "power4.inOut"
+    },'b')
+  
 }
 function page2(){
-
+    const tl = gsap.timeline({
+       
+      });
+      gsap.to(".page2",{
+        opacity:1,
+        duration: 1,
+        // ease: "power4.inOut"
+         scrollTrigger: {
+          trigger: ".page2",
+          scroller: "#main", // Attach the scroll to the Locomotive Scroll container
+          toggleActions: "restart none none reverse",
+          start: "top 0%",
+          end: "top -80%",
+        //   pin: true,
+          
+        }
+      })
 }
 function page3(){
 
 }
+page1()
+page2()
